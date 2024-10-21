a318c12eba0ecbbe34667eda71c2241e1048eedc
BEN150200 <bielescorsell@gmail.com>
2024-10-21 13:23:50 +0200

    some tests in backend with websockets

---
+++ backend/src/api/routers/get-character.ts
@@ -0,0 +1,23 @@
+import { Character } from "../../core/models/CharacterModels";
+import { CharacterModel } from "../../core/schemas/CharacterSchemas";
+
+export async function _getCharacter(character: string) {
+  return await CharacterModel.findOne({
+    name: character,
+  });
+}
+
+export async function _putCharacter(character: string, data: Character) {
+  await CharacterModel.findOneAndUpdate(
+    {
+      name: character,
+    },
+    {
+      ...data,
+    },
+    {
+      upsert: true,
+      new: true,
+    }
+  );
+}
