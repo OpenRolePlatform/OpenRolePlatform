fadfb2a90d864c83b4b38b02eb5feea1dc2ffa47
BEN150200 <bielescorsell@gmail.com>
2024-10-26 16:32:23 +0200

    feat(backend): :sparkles: list the databases when connecting to mongodb

--- backend/src/connectDB.ts
+++ backend/src/connectDB.ts
@@ -5,13 +5,21 @@ const dbHost = process.env.DB_HOST;
 const dbName = process.env.DB_NAME;
 const dbPort = process.env.DB_PORT;
 
-export const db = mongoose
-  .connect(`mongodb://${dbHost}:${dbPort}/${dbName}`)
-  .then((res) => {
-    if (res) {
-      console.log(`Database connection successfully to ${dbName}`);
-    }
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoose.set("strictQuery", true);
+
+export const db = mongoose.createConnection(
+  `mongodb://${dbHost}:${dbPort}/${dbName}`
+);
+
+// Check DB Connection
+db.once("open", () => {
+  (async () => {
+    /* const data = await mongoose?.connection?.db?.admin().command({
+      listDatabases: 1,
+    }); */
+    const dbList = await db.listDatabases();
+    console.log("List of databases: ");
+    console.log(dbList);
+  })();
+  console.log(`Database connection successfully to ${dbName}`);
+});
